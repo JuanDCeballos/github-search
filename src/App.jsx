add4dc74be6a46e3731775b8922239eb6014097b
Juan Ceballos <juandavi789@hotmail.com>
2023-12-17 01:15:09 -0500

    Add inital logic for ligth and dark mode, import icons, add some structure and pass data to the components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import UserInfo from './components/UserInfo';
 import { useState } from 'react';
 import './App.css';
 
+import { MdLightMode } from 'react-icons/md'; // <MdLightMode />
+import { MdDarkMode } from 'react-icons/md'; // <MdDarkMode />
+
 const App = () => {
   const [colorMode, setColorMode] = useState(false);
   const [userInfo, setUserInfo] = useState(null);
@@ -14,10 +17,21 @@ const App = () => {
   console.log(userInfo);
 
   return (
-    <div>
-      <h1>Hello world!</h1>
-      <Search setUserInfo={setUserInfo} />
-      <UserInfo userInfo={userInfo} />
+    <div className='app'>
+      <main className='container'>
+        <h1 className='title'>Devfinder</h1>
+        {colorMode ? (
+          <button onClick={() => setColorMode((prevColor) => !prevColor)}>
+            Light <MdLightMode />
+          </button>
+        ) : (
+          <button onClick={() => setColorMode((prevColor) => !prevColor)}>
+            Dark <MdDarkMode />
+          </button>
+        )}
+        <Search setUserInfo={setUserInfo} mode={colorMode} />
+        <UserInfo userInfo={userInfo} mode={colorMode} />
+      </main>
     </div>
   );
 };
