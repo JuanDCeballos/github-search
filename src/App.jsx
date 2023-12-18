7569feb8fab510a88f7efb8b6f2a424b2acef80e
Juan Ceballos <juandavi789@hotmail.com>
2023-12-17 23:09:42 -0500

    Add structure to the html  App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import UserInfo from './components/UserInfo';
 import { useState } from 'react';
 import './App.css';
 
-import { MdLightMode } from 'react-icons/md'; // <MdLightMode />
-import { MdDarkMode } from 'react-icons/md'; // <MdDarkMode />
+import { MdLightMode } from 'react-icons/md';
+import { MdDarkMode } from 'react-icons/md';
 
 const App = () => {
   const [colorMode, setColorMode] = useState(false);
@@ -14,21 +14,21 @@ const App = () => {
     setColorMode((prevMode) => !prevMode);
   };
 
-  console.log(userInfo);
-
   return (
     <div className='app'>
       <main className='container'>
-        <h1 className='title'>Devfinder</h1>
-        {colorMode ? (
-          <button onClick={() => setColorMode((prevColor) => !prevColor)}>
-            Light <MdLightMode />
-          </button>
-        ) : (
-          <button onClick={() => setColorMode((prevColor) => !prevColor)}>
-            Dark <MdDarkMode />
-          </button>
-        )}
+        <div>
+          <h1 className='title'>Devfinder</h1>
+          {colorMode ? (
+            <button onClick={() => setColorMode((prevColor) => !prevColor)}>
+              Light <MdLightMode />
+            </button>
+          ) : (
+            <button onClick={() => setColorMode((prevColor) => !prevColor)}>
+              Dark <MdDarkMode />
+            </button>
+          )}
+        </div>
         <Search setUserInfo={setUserInfo} mode={colorMode} />
         <UserInfo userInfo={userInfo} mode={colorMode} />
       </main>
