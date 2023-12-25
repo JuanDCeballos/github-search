75febf46fe0175d7ff4ba56e8b49522ded6370ae
Juan Ceballos <juandavi789@hotmail.com>
2023-12-24 22:45:56 -0500

    Add structure to UserInfo component with some html clases for later use in css

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -3,46 +3,7 @@ import { FaXTwitter } from 'react-icons/fa6';
 import { HiBuildingOffice2 } from 'react-icons/hi2';
 import { ImLink } from 'react-icons/im';
 
-const UserInfo = () => {
-  const userInfo = {
-    avatar_url: 'https://avatars.githubusercontent.com/u/80058433?v=4',
-    bio: null,
-    blog: '',
-    company: null,
-    created_at: '2021-03-04T16:32:59Z',
-    email: null,
-    events_url: 'https://api.github.com/users/JuanDCeballos/events{/privacy}',
-    followers: 6,
-    followers_url: 'https://api.github.com/users/JuanDCeballos/followers',
-    following: 3,
-    following_url:
-      'https://api.github.com/users/JuanDCeballos/following{/other_user}',
-    gists_url: 'https://api.github.com/users/JuanDCeballos/gists{/gist_id}',
-    gravatar_id: '',
-    hireable: null,
-    html_url: 'https://github.com/JuanDCeballos',
-    id: 80058433,
-    location: 'Medellín - Colombia',
-    login: 'JuanDCeballos',
-    name: null,
-    node_id: 'MDQ6VXNlcjgwMDU4NDMz',
-    organizations_url: 'https://api.github.com/users/JuanDCeballos/orgs',
-    public_gists: 0,
-    public_repos: 33,
-    received_events_url:
-      'https://api.github.com/users/JuanDCeballos/received_events',
-    repos_url: 'https://api.github.com/users/JuanDCeballos/repos',
-    site_admin: false,
-    starred_url:
-      'https://api.github.com/users/JuanDCeballos/starred{/owner}{/repo}',
-    subscriptions_url:
-      'https://api.github.com/users/JuanDCeballos/subscriptions',
-    twitter_username: null,
-    type: 'User',
-    updated_at: '2023-08-28T15:47:08Z',
-    url: 'https://api.github.com/users/JuanDCeballos',
-  };
-
+const UserInfo = ({ userInfo, mode }) => {
   const formatDate = (dateString) => {
     // Create a Date object from the input string
     const date = new Date(dateString);
@@ -79,35 +40,47 @@ const UserInfo = () => {
   return (
     <>
       {userInfo ? (
-        <div className='user-info-container'>
+        <div
+          className={`user-info-container user-info-container-${
+            mode ? `dark` : `light`
+          }`}
+        >
           <div className='desc-container'>
             <img src={userInfo.avatar_url} className='profile-picture' />
             <div className='desc-container-info'>
               <div className='names'>
-                <p>{userInfo.login}</p>
-                <p>{userInfo.name ? userInfo.name : ''}</p>
+                <p className={`login login-${mode ? `dark` : `light`}`}>
+                  {userInfo.login}
+                </p>
+                <p className='name'>{userInfo.name ? userInfo.name : ''}</p>
               </div>
-              <p>Joined {formatDate(userInfo.created_at.substring(0, 10))}</p>
+              <p className='joined'>
+                Joined {formatDate(userInfo.created_at.substring(0, 10))}
+              </p>
             </div>
           </div>
           <div className='bio-container'>
             {userInfo.bio ? (
               <p className='bio'>{userInfo.bio}</p>
             ) : (
-              <p className='bio'>This profile has no bio.</p>
+              <p className='bio'>This profile has no bio</p>
             )}
           </div>
-          <div className='stats-container'>
+          <div
+            className={`stats-container stats-container-${
+              mode ? `dark` : `light`
+            }`}
+          >
             <div>
-              <p>Repos</p>
+              <p className='stat-text'>Repos</p>
               <p className='stat'>{userInfo.public_repos}</p>
             </div>
             <div>
-              <p>Followers</p>
+              <p className='stat-text'>Followers</p>
               <p className='stat'> {userInfo.followers}</p>
             </div>
             <div>
-              <p>Following</p>
+              <p className='stat-text'>Following</p>
               <p className='stat'> {userInfo.following}</p>
             </div>
           </div>
